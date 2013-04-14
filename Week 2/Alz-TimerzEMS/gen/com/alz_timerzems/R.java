834585e399b56979cb7cdafe87371f9123a94b73
Janelle Salisbury <janellesalisbury@fullsail.edu>
2013-04-13 20:24:51 -0400

    Log in page almost completed

--- Week 2/Alz-TimerzEMS/gen/com/alz_timerzems/R.java
+++ Week 2/Alz-TimerzEMS/gen/com/alz_timerzems/R.java
@@ -10,26 +10,36 @@ package com.alz_timerzems;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int paleblue=0x7f050000;
+        public static final int red=0x7f050001;
+    }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int clock=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int menu_settings=0x7f070004;
-        public static final int tab1=0x7f070000;
-        public static final int tab2=0x7f070001;
-        public static final int tab3=0x7f070002;
-        public static final int tab4=0x7f070003;
+        public static final int atlogo=0x7f080004;
+        public static final int menu_settings=0x7f080006;
+        public static final int password=0x7f080000;
+        public static final int textView1=0x7f080002;
+        public static final int title=0x7f080005;
+        public static final int username=0x7f080001;
+        public static final int userpassword=0x7f080003;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
     }
     public static final class menu {
-        public static final int activity_main=0x7f060000;
+        public static final int activity_main=0x7f070000;
     }
     public static final class string {
+        public static final int alz_timerz_ems=0x7f040005;
         public static final int app_name=0x7f040000;
         public static final int hello_world=0x7f040001;
         public static final int menu_settings=0x7f040002;
+        public static final int password=0x7f040004;
+        public static final int username_=0x7f040003;
     }
     public static final class style {
         /** 
@@ -53,10 +63,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f050000;
+        public static final int AppBaseTheme=0x7f060000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f050001;
+        public static final int AppTheme=0x7f060001;
     }
 }
