17f84a547cfc249f0a3f6bfcb4595cb480c9355f
Janelle Salisbury <janellesalisbury@fullsail.edu>
2013-04-18 13:44:44 -0400

    Network Connectivity

--- Week 2/Alz-TimerzEMS/src/com/alz_timerzems/MainActivity.java
+++ Week 2/Alz-TimerzEMS/src/com/alz_timerzems/MainActivity.java
@@ -1,12 +1,15 @@
 package com.alz_timerzems;
 
+import android.net.ConnectivityManager;
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 	
@@ -17,6 +20,15 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		//CHECK FOR NETWORK CONNECTIVITY
+		ConnectivityManager conmgr = (ConnectivityManager)getApplicationContext().getSystemService(Context.CONNECTIVITY_SERVICE);
+		if(conmgr !=null && (conmgr.getNetworkInfo(1).isAvailable() == true) || (conmgr.getNetworkInfo(0).isAvailable() == true)){
+			//do something
+		}else{
+			Toast toast = Toast.makeText(getApplicationContext(), "No Network Available", Toast.LENGTH_SHORT);
+			toast.show();
+		}
 		
 		//FOR TESTING PURPOSES TO MAKE SURE THE VIEW IS SET UP CORRECTLY WITH THE TABS
 		login = (Button) findViewById(R.id.login);
