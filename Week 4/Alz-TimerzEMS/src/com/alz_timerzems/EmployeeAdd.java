c1914023cdefedd12d644076b171bab5cac41808
Janelle Salisbury <janellesalisbury@fullsail.edu>
2013-05-01 10:34:48 -0400

    Save finally working

--- Week 4/Alz-TimerzEMS/src/com/alz_timerzems/EmployeeAdd.java
+++ Week 4/Alz-TimerzEMS/src/com/alz_timerzems/EmployeeAdd.java
@@ -1,5 +1,6 @@
 package com.alz_timerzems;
 
+import com.parse.Parse;
 import com.parse.ParseObject;
 
 import android.app.Activity;
@@ -9,13 +10,14 @@ import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class EmployeeAdd extends Activity{
-	String _name;
-	String _phone;
-	String _address;
-	String _mobile;
-	String _email;
+	EditText _name;
+	EditText _phone;
+	EditText _address;
+	EditText _mobile;
+	EditText _email;
 	Button saveInfo;
 	
 	@Override
@@ -23,13 +25,16 @@ public class EmployeeAdd extends Activity{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.employee_activity);
 		
+		//INITIALIZE PARSE
+		Parse.initialize(this, "9A7rNDmuRqbEQAkuK8CsvTKqXwJ8neVE6ZYPpJOz", "MQOtWDlSgfllhr1L91oy8VfrPWuBEbNePtCVFgzu"); 
+
+		
 		//Passed imformation
-		Intent employeeInfo = getIntent();
-		_name = employeeInfo.getStringExtra("Name");
-		_phone = employeeInfo.getStringExtra("Phone");
-		_address = employeeInfo.getStringExtra("Address");
-		_mobile = employeeInfo.getStringExtra("Mobile");
-		_email = employeeInfo.getStringExtra("Email");
+		_name = (EditText) findViewById(R.id.editText1);
+		_phone = (EditText) findViewById(R.id.editText2);
+		_address = (EditText) findViewById(R.id.editText3);
+		_mobile = (EditText) findViewById(R.id.editText4);
+		_email = (EditText) findViewById(R.id.editText5);
 		
 		saveInfo = (Button) findViewById(R.id.save);
 		saveInfo.setOnClickListener(new OnClickListener() {
@@ -38,11 +43,16 @@ public class EmployeeAdd extends Activity{
 			public void onClick(View v) {
 				ParseObject savedEmployee = new ParseObject("SavedEmployee");
 				savedEmployee.put("row", "value");
-				savedEmployee.put("Name", _name);
-				savedEmployee.put("Phone", _phone);
-				savedEmployee.put("Address", _address);
-				savedEmployee.put("Mobile", _mobile);
-				savedEmployee.put("Email", _email);
+				savedEmployee.put("Name", _name.getText().toString());
+				savedEmployee.put("Phone", _phone.getText().toString());
+				savedEmployee.put("Address", _address.getText().toString());
+				savedEmployee.put("Mobile", _mobile.getText().toString());
+				savedEmployee.put("Email", _email.getText().toString());
+				savedEmployee.saveInBackground();
+				
+				//INFORMATION THAT WILL BE SENT
+				Intent home = new Intent(EmployeeAdd.this, TabActivity.class);
+				startActivity(home);
 				
 				
 			}
