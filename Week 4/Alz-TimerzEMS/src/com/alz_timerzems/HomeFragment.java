05a69147163420b35cb08dd1a3830d6cb4443a59
Janelle Salisbury <janellesalisbury@fullsail.edu>
2013-05-01 15:30:29 -0400

    save working, listview not so much

--- Week 4/Alz-TimerzEMS/src/com/alz_timerzems/HomeFragment.java
+++ Week 4/Alz-TimerzEMS/src/com/alz_timerzems/HomeFragment.java
@@ -1,5 +1,15 @@
 package com.alz_timerzems;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import com.parse.FindCallback;
+import com.parse.Parse;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+
 import android.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
@@ -10,6 +20,7 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.RelativeLayout;
+import android.widget.SimpleAdapter;
 
 public class HomeFragment extends Fragment{
 	//GLOBAL VARIABLES
@@ -23,7 +34,7 @@ public class HomeFragment extends Fragment{
 	public void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
 		
-		
+	
 	}
 	
 
@@ -32,7 +43,11 @@ public class HomeFragment extends Fragment{
 			Bundle savedInstanceState) {
 
 		   _view =  (RelativeLayout) inflater.inflate(R.layout.home_tab, container, false);
-
+		   
+			//INITIALIZE PARSE
+			Parse.initialize(null, "9A7rNDmuRqbEQAkuK8CsvTKqXwJ8neVE6ZYPpJOz", "MQOtWDlSgfllhr1L91oy8VfrPWuBEbNePtCVFgzu");
+			
+		
 		 //SET BUTTONS (ADD AND OK) AND LISTVIEW
 		   _add_btn = (Button) _view.findViewById(R.id.add);
 		   _ok_btn = (Button) _view.findViewById(R.id.ok);
@@ -51,8 +66,46 @@ public class HomeFragment extends Fragment{
 			}
 		});
 
+		   ParseQuery query = new ParseQuery("SavedEmployee");
+		   query.whereEqualTo("row", "value");
+		   query.findInBackground(new FindCallback() {
+			
+			@Override
+			public void done(List<ParseObject> contactList, ParseException e) {
+				if(e==null){
+					initListView(contactList);
+				}else{
+					objectRetrievalFailed();
+				}
 				
+			}
+		});
+		   
+		
 		return _view;
 	}
+	
+	   protected void objectRetrievalFailed(){
+		   
+	   }
+	   
+	   private void initListView(List<ParseObject> object){
+		   ArrayList<HashMap<String, String>> list = new ArrayList<HashMap<String, String>>(2);
+		   HashMap<String, String> obj;
+		   
+		   for(ParseObject hashMap: object){
+			   obj = new HashMap<String,String>();
+			   obj.put("line1", hashMap.getString("Name"));
+			   obj.put("line2", hashMap.getString("Mobile"));
+			   list.add(obj);
+		   }
+		   
+		   String[] line = {"line1", "line2"};
+		   int[] display = {R.id.text1};
+		   
+		   //create array adapter for listview
+		   SimpleAdapter listAdapter = new SimpleAdapter(HomeFragment.this, list, R.layout.list_item, line, display);
+		   employees.setAdapter(listAdapter);
+	   }
 
 }
