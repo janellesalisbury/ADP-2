0920f3214842d2cdd240ae14757ef02078fa473d
Janelle Salisbury <janellesalisbury@fullsail.edu>
2013-05-13 17:33:02 -0400

    Beginning Custom ListViews

---
+++ Week 2/Alz-TimerzEMS/src/com/alz_timerzems/HomeFragment.java
@@ -0,0 +1,98 @@
+package com.alz_timerzems;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import com.parse.FindCallback;
+import com.parse.Parse;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+
+import android.app.Fragment;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
+
+public class HomeFragment extends Fragment{
+	//GLOBAL VARIABLES
+	Button _add_btn;
+	Button _ok_btn;
+	ListView employees;
+	static View _view;
+	
+	
+	@Override 
+	public void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+		
+	
+	}
+	
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+
+		   _view =  (RelativeLayout) inflater.inflate(R.layout.home_tab, container, false);
+		   
+			//INITIALIZE PARSE
+			Parse.initialize(null, "9A7rNDmuRqbEQAkuK8CsvTKqXwJ8neVE6ZYPpJOz", "MQOtWDlSgfllhr1L91oy8VfrPWuBEbNePtCVFgzu");
+			
+		
+		 //SET BUTTONS (ADD AND OK) AND LISTVIEW
+		   _add_btn = (Button) _view.findViewById(R.id.add);
+		   _ok_btn = (Button) _view.findViewById(R.id.ok);
+		   employees = (ListView) _view.findViewById(R.id.listView1);
+		   
+		   //ADD BUTTON FUNCTIONALITY
+		   _add_btn.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Intent addForm = new Intent();
+				addForm.setClass(getActivity(), EmployeeAdd.class);
+				startActivity(addForm);				
+		   
+				
+			}
+		});
+
+		   ParseQuery query = new ParseQuery("SavedEmployee");
+		   query.whereEqualTo("row", "value");
+		   query.findInBackground(new FindCallback() {
+			
+			@Override
+			public void done(List<ParseObject> employeeList, ParseException e) {
+				if(e == null){
+					initListView(employeeList);
+				}else{
+					objectRetrievalFailed();
+				}
+				
+			}
+		});
+		return _view;
+	}
+
+	protected void objectRetrievalFailed(){
+		
+	}
+	private void initListView(List<ParseObject> objects){
+		ArrayList<HashMap<String, String>> list = new ArrayList<HashMap<String, String>>(2);
+		HashMap<String, String> names;
+		for(ParseObject hashMap: objects){
+			names = new HashMap<String, String>();
+			names.put("line 1", hashMap.getString("Name"));
+			names.put("line 2", hashMap.getString("Phone"));
+			list.add(names);
+		}
+	}
+}
