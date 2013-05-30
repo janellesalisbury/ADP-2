3a69c8bd3f7a179b047d4106433890b9d24c4a2f
Janelle Salisbury <janellesalisbury@fullsail.edu>
2013-05-30 17:42:25 -0400

    finishing up clean up...

--- Week 4/Alz-TimerzEMS EmployeeVersion/gen/com/alz_timerzems/R.java
+++ Week 4/Alz-TimerzEMS EmployeeVersion/gen/com/alz_timerzems/R.java
@@ -15,11 +15,12 @@ public final class R {
         public static final int red=0x7f050001;
     }
     public static final class drawable {
-        public static final int calendar=0x7f020000;
-        public static final int clock=0x7f020001;
-        public static final int ic_launcher=0x7f020002;
-        public static final int icon=0x7f020003;
-        public static final int maps=0x7f020004;
+        public static final int atfooter=0x7f020000;
+        public static final int calendar=0x7f020001;
+        public static final int clock=0x7f020002;
+        public static final int ic_launcher=0x7f020003;
+        public static final int icon=0x7f020004;
+        public static final int maps=0x7f020005;
     }
     public static final class id {
         public static final int address=0x7f08000b;
--- Week 4/Alz-TimerzEMS/src/com/alz_timerzems/EmployeeDetails.java
+++ Week 4/Alz-TimerzEMS/src/com/alz_timerzems/EmployeeDetails.java
@@ -120,10 +120,9 @@ public class EmployeeDetails extends Activity{
 			}
 		});
 		
-		
 	
 	}	
-			
+		
 		
 	private void call(){
 		try{
